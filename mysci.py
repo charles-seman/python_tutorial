9524f1b0a7dab6eeac13ee5fef0f770c130b4fdb
Charles Seman <Charles.Seman@noaa.gov>
2020-10-28 15:58:06 -0400

    refactoring data parsing code

--- mysci.py
+++ mysci.py
@@ -2,49 +2,16 @@
 
 filename = "data/wxobs20170821.txt"
 
-#----------#
-# Method 1 #
-#----------#
+# Column names and column indices to read
+columns = {'date':0, 'time':1, 'tempout':2}
 
-#datafile = open(filename, 'r')
+# Data types for each column (only if non-string)
+types = {'tempout':float}
 
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
-
-#datafile.close()
-
-#----------#
-# Method 2 #
-#----------#
-
-#datafile = open(filename, 'r')
-
-#data = datafile.read()
-
-#datafile.close()
-
-#----------#
-# Method 3 #
-#----------#
-
-# following "context manager" is recommended since it closes datafile for you
-
-#with open(filename, 'r') as datafile:
-#  data = datafile.read()
-
-# DEBUG
-
-#print(data)
-#print(type(data))
-#print("data")
-
-#-------------------------------------------------
-
-data = {'date':[], 'time':[], 'tempout':[]}
-
-#time = data['time']
+# Initialize my data variable
+data = {}
+for column in columns:
+    data[column] = []
 
 with open(filename, 'r') as datafile:
 
@@ -57,6 +24,12 @@ with open(filename, 'r') as datafile:
     # read and parse the rest of the file
     for line in datafile:
         split_line = line.split() #(',') or ('/t')
-        data['date'].append(split_line[0])
-        data['time'].append(split_line[1])
-        data['tempout'].append(float(split_line[2]))
+        for column in columns:
+            i = columns[column]
+            t = types.get(column,str)
+            value = t(split_line[i])
+            data[column].append(value)
+
+# DEBUG
+
+print(data['tempout'])
